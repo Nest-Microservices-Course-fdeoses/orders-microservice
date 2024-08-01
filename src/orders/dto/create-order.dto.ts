0497fe63ba4c305f0fa9e1b1ac98d35cc87ae427
fdeoses <frdeosesfuentelu@gmail.com>
2024-08-01 14:20:01 +0200

    Update dto and save in BBDD

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,29 @@
-export class CreateOrderDto {}
+import { OrdersStatus } from '@prisma/client';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+import { OrderStatusList } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalTime: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Possible status values are ${OrderStatusList}`,
+  })
+  @IsOptional()
+  status: OrdersStatus = OrdersStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
